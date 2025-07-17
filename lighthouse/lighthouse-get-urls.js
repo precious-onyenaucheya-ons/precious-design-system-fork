83a1ef495114efc31179a2d056365b00e2d97f20
Precious Onyenaucheya <precious.onyenaucheya@ons.gov.uk>
2025-07-17 14:29:24 +0100

    make lighthouse fail

--- lighthouse/lighthouse-get-urls.js
+++ lighthouse/lighthouse-get-urls.js
@@ -30,11 +30,8 @@ async function getUrls() {
     ];
     // collect all the examples fail at 'aria-allowed-attr' audit check
     const knownIssueFiles = [
-        'example-errors-proto.html',
         'example-errors-proto-errors.html',
         'example-feedback-form.html',
-        'example-feedback-form-errors.html',
-        'example-radios-with-revealed-text-input.html',
         'example-radios-with-revealed-text-input-expanded.html',
         'example-radios-with-revealed-text-area.html',
         'example-radios-with-revealed-text-area-expanded.html',
@@ -46,8 +43,6 @@ async function getUrls() {
         'example-radios-with-revealed-checkboxes-expanded.html',
         'example-radios-with-clear-button.html',
         'example-radios-with-clear-button-expanded.html',
-        'example-accordion.html',
-        'example-button-custom.html',
         'example-button-download.html',
     ];
     for (const directory of directories) {
